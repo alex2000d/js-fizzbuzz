d2579d4922bb768427b20815c77d48ebad3cb384
alex2000d <demarcoalessandro777@gmail.com>
2024-06-27 17:23:55 +0200

    [UPD] created print for fizzbuzz , fizz and buzz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
 // creo un ciclo che mi stampi numeri da 1 a 100
-for (let i = 0; i <= 100; i++){ 
-    console.log(i);
+for (let i = 0; i <= 100; i++){
+    
+
+    // se è multiplo sia di 3 che di 5 assegno la stringa fizzbuzz
+        if ((i % 3 == 0) && (i % 5 == 0)){
+          console.log( i,'fizzbuzz')
+         
+     }
+    //  se è multiplo di 3 assegno la stringa fizz
+         else if (i % 3 == 0){
+        console.log(i,'fizz')
+         
+        }
+        // se è multiplo di 5 assegno la stringa buzz
+        else if (i % 5 == 0){
+            console.log(i,'buzz')
+             
+         }
+        //  altrimenti stampa solo i numeri
+         else if (i % 2 == 0){
+            console.log(i)
+             
+         }
+    
+         
+       
     }
