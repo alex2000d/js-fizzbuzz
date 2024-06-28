edff76502acc9a198429d542faf51418a152dbcb
alex2000d <demarcoalessandro777@gmail.com>
2024-06-28 15:32:28 +0200

    [UPD] fix the javascript

--- js/script.js
+++ js/script.js
@@ -14,25 +14,25 @@ square.classList.add('square')
     // se è multiplo sia di 3 che di 5 assegno la stringa fizzbuzz
         if ((i % 3 == 0) && (i % 5 == 0)){
           console.log( i,'fizzbuzz')
-         square.innerText = ( i,'fizzbuzz');
+         square.innerText = 'fizzbuzz';
          square.classList.add('red')
-     }
+        }
     //  se è multiplo di 3 assegno la stringa fizz
-         else if (i % 3 == 0){
-        console.log(i,'fizz')
-        square.innerText = ( i,'fizz');
-        square.classList.add('green')
+        else if (i % 3 == 0){
+          console.log(i,'fizz')
+          square.innerText = 'fizz';
+          square.classList.add('green')
         }
         // se è multiplo di 5 assegno la stringa buzz
         else if (i % 5 == 0){
             console.log(i,'buzz')
-            square.innerText = ( i,'buzz');
+            square.innerText = 'buzz';
             square.classList.add('yellow')
          }
         //  altrimenti stampa solo i numeri
-         else if (i % 1 == 0){
+         else {
             console.log(i)
-            square.innerText = (i);
+            square.innerText = i;
             square.classList.add('blu')
          }
     
